21aba792cda83633701d47d98844ce16f18419c1
Pojan <86289506+xXpoyanXx@users.noreply.github.com>
2024-02-15 12:56:55 +0100

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,24 +6,28 @@ import ProfileButtonImg from '../../Assets/Images/profile_blank.png';
 
 export default function Footer() {
   return (
-    <MDBFooter bgColor='light' className='text-center text-lg-start text-muted'>
-      <MDBContainer fluid className='d-flex justify-content-between align-items-center p-3 text-center' style={{ backgroundColor: '#AA222B', height: '70px' }}>
-        <div className='col-4'>
-          <a href='../' className='text-reset'>
-            <img src={BackButtonImg} alt='Back' style={{ maxHeight: '25%', maxWidth: '25%' }} />
-          </a>
-        </div>
-        <div className='col-4'>
-          <a href='../' className='text-reset'>
-            <img src={HomeButtonImg} alt='Home' style={{ maxHeight: '25%', maxWidth: '25%' }} />
-          </a>
-        </div>
-        <div className='col-4'>
-          <a href='myjobs' className='text-reset'>
-            <img src={ProfileButtonImg} alt='MyJobs' style={{ maxHeight: '25%', maxWidth: '25%' }} />
-          </a>
-        </div>
-      </MDBContainer>
-    </MDBFooter>
+    <>
+      <div style={{ paddingBottom: '70px' }}>
+      </div>
+      <MDBFooter bgColor='light' className='text-center text-lg-start text-muted' style={{ position: 'fixed', bottom: 0, left: 0, width: '100%' }}>
+        <MDBContainer fluid className='d-flex justify-content-between align-items-center p-3 text-center' style={{ backgroundColor: '#AA222B', height: '70px' }}>
+          <div className='col-4'>
+            <a href='../' className='text-reset'>
+              <img src={BackButtonImg} alt='Back' style={{ maxHeight: '25%', maxWidth: '25%' }} />
+            </a>
+          </div>
+          <div className='col-4'>
+            <a href='../' className='text-reset'>
+              <img src={HomeButtonImg} alt='Home' style={{ maxHeight: '25%', maxWidth: '25%' }} />
+            </a>
+          </div>
+          <div className='col-4'>
+            <a href='myjobs' className='text-reset'>
+              <img src={ProfileButtonImg} alt='MyJobs' style={{ maxHeight: '25%', maxWidth: '25%' }} />
+            </a>
+          </div>
+        </MDBContainer>
+      </MDBFooter>
+    </>
   );
 }
