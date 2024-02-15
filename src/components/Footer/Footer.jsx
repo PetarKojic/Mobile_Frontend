0f0682fd1ce24f4c41a3bbda9bcce11146ac3be8
Pojan <86289506+xXpoyanXx@users.noreply.github.com>
2024-02-15 10:59:54 +0100

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,16 +7,22 @@ import ProfileButtonImg from '../../Assets/Images/profile_blank.png';
 export default function Footer() {
   return (
     <MDBFooter bgColor='light' className='text-center text-lg-start text-muted' style={{ position: 'fixed', bottom: 0, left: 0, width: '100%' }}>
-      <MDBContainer fluid className='d-flex justify-content-between align-items-center p-3 text-center' style={{ backgroundColor: '#AA222B', height: '100px' }}>
-        <a href='../' className='text-reset'>
-          <img src={BackButtonImg} alt='Back' style={{ maxHeight: '20%', maxWidth: '20%' }} />
-        </a>
-        <a href='../' className='text-reset'>
-          <img src={HomeButtonImg} alt='Home' style={{ maxHeight: '20%', maxWidth: '20%' }} />
-        </a>
-        <a href='myjobs' className='text-reset'>
-          <img src={ProfileButtonImg} alt='MyJobs' style={{ maxHeight: '17%', maxWidth: '17%' }} />
-        </a>
+      <MDBContainer fluid className='d-flex justify-content-between align-items-center p-3 text-center' style={{ backgroundColor: '#AA222B', height: '70px' }}>
+        <div className='col-4'>
+          <a href='../' className='text-reset'>
+            <img src={BackButtonImg} alt='Back' style={{ maxHeight: '20%', maxWidth: '20%' }} />
+          </a>
+        </div>
+        <div className='col-4'>
+          <a href='../' className='text-reset'>
+            <img src={HomeButtonImg} alt='Home' style={{ maxHeight: '20%', maxWidth: '20%' }} />
+          </a>
+        </div>
+        <div className='col-4'>
+          <a href='myjobs' className='text-reset'>
+            <img src={ProfileButtonImg} alt='MyJobs' style={{ maxHeight: '15%', maxWidth: '15%' }} />
+          </a>
+        </div>
       </MDBContainer>
     </MDBFooter>
   );
